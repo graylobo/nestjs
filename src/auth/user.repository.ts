371a3bb5abcf89e39282cec4b8ba71cb3db1610f
graylobo <cazador.min@gmail.com>
2023-08-02 01:44:17 +0900

    엔티티 컬럼에 유니크 값주기(4:09:55)

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,12 +2,27 @@ import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 import { CustomRepository } from 'src/database/typeorm-ex.decorator';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialDto;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      console.log(error);
+      switch (error.code) {
+        case '23505':
+          throw new ConflictException('이미존재함');
+        default:
+          throw new InternalServerErrorException('뷁');
+      }
+    }
   }
 }
