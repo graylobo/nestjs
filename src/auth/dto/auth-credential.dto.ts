5fcc67b7d81454292fc4f1083e396a0f2eb8d116
graylobo <cazador.min@gmail.com>
2023-08-02 01:01:53 +0900

    유저데이터 유효성 체크(4:03:55)

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4, { message: '최소4이상' })
+  @MaxLength(20)
+  @Matches(/^[a-zA-z0-9]*$/, {
+    message: '영어랑 숫자만 ㄱ',
+  })
   password: string;
 }
